45eef4a9249bbd18548546bf143dec8b75cdac1f
Roland <fun.redoc@gmail.com>
2014-08-19 23:43:18 +0200

    2014.08.19 23:43

--- SpriteKitBlaster/RSTurretEntity.m
+++ SpriteKitBlaster/RSTurretEntity.m
@@ -2,8 +2,8 @@
 //  RSTurretEntity.m
 //  SpriteKitBlaster
 //
-//  Created by Roland Stellmach on 19.08.14.
-//  Copyright (c) 2014 Roland Stellmach. All rights reserved.
+//  Created by Fun Fun on 19.08.14.
+//  Copyright (c) 2014 Fun Fun. All rights reserved.
 //
 
 #import "RSTurretEntity.h"
